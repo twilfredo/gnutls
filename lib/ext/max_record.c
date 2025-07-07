4f85f80f8d4e2d03d98867784cd85f2fc5988084
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2025-07-07 13:21:58 +1000

    ext/max_record: add function to get max send size
    Adds a new function to max_record library to extract
    the endpoints maximum record send size. Which may have been negotiated
    through the record_size_limit or the max_fragment_length extensions.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- lib/ext/max_record.c
+++ lib/ext/max_record.c
@@ -307,3 +307,20 @@ ssize_t gnutls_record_set_max_recv_size(gnutls_session_t session, size_t size)
 
 	return 0;
 }
+
+/**
+ * gnutls_record_get_max_send_size:
+ * @session: is a #gnutls_session_t type.
+ *
+ * Get the max record send size.
+ *
+ * Returns: On success, the negotiated record size limit,
+ *   If the server record size limit is not specified an error
+ *   is returned.
+ *
+ * Since: 3.8.10
+ **/
+size_t gnutls_record_get_max_send_size(gnutls_session_t session)
+{
+	return max_record_send_size(session);
+}
