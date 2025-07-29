ddb1c5bcc83dddb1ed742a51fe795e4b69039b1e
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2025-07-29 14:48:12 +1000

    [HACK] client-hello/padding: debugs
    Force ClientHello padding extension without hacing to use the compat
    macros which seems to disable other security features for legacy
    support/maximum compatibility.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- lib/ext/dumbfw.c
+++ lib/ext/dumbfw.c
@@ -55,24 +55,26 @@ static int _gnutls_dumbfw_send_params(gnutls_session_t session,
 				      gnutls_buffer_st *extdata)
 {
 	int total_size = 0, ret;
-	uint8_t pad[257];
-	unsigned pad_size;
+	unsigned pad_size = 64;
+	uint8_t pad[pad_size];
 	ssize_t len = extdata->length - sizeof(mbuffer_st);
 
+	_gnutls_handshake_log("wmk: _gnutls_dumbfw_send_params");
+
+	_gnutls_handshake_log("wmk: dumbfw: %d | DTLS: %d", session->internals.dumbfw, IS_DTLS(session));
+	_gnutls_handshake_log("wmk: %ld", len);
+
 	if (session->security_parameters.entity == GNUTLS_SERVER ||
-	    session->internals.dumbfw == 0 || IS_DTLS(session) != 0 ||
-	    (len < 256 || len >= 512)) {
+	    IS_DTLS(session) != 0) {
+		_gnutls_handshake_log("wmk: no padding!");
 		return 0;
 	} else {
-		/* 256 <= extdata->length < 512 */
-		pad_size = 512 - len;
 		memset(pad, 0, pad_size);
-
 		ret = gnutls_buffer_append_data(extdata, pad, pad_size);
 		if (ret < 0)
 			return gnutls_assert_val(ret);
-
 		total_size += pad_size;
+		_gnutls_handshake_log("wmk: len: %ld | psize: %d | tsize: %d", len, pad_size, total_size);
 	}
 
 	return total_size;
